2ff7732f2d50ec7c5009278f559ec36573cd12db
Marcelo Ribeiro <marceloribeiromidia@gmail.com>
2023-06-06 13:50:14 -0300

    Refactoring

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 import express from "express";
-import { downloadHighres } from "./index.js";
+import youtube from "./index.js";
 const app = express();
 
 app.get("/:videoId", async (req, res) => {
   const { videoId } = req.params;
-  downloadHighres(videoId, res);
+  youtube.downloadHighres(videoId, res);
 });
 
 app.listen(3000, () => {
